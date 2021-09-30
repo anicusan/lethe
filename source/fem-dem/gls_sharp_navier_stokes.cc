fd9dd6260ae047fbae543a6f5a4192acde0b8fd4
Lucka <luckabarbeau@gmail.com>
2021-09-30 15:12:38 -0400

    corected bug and added nonlinear tol for particle

--- source/fem-dem/gls_sharp_navier_stokes.cc
+++ source/fem-dem/gls_sharp_navier_stokes.cc
@@ -1784,6 +1784,8 @@ template <int dim>
 void
 GLSSharpNavierStokesSolver<dim>::integrate_particles()
 {
+  dem_particles = particles;
+  particle_residual=0;
 
   particle_residual=0;
   ib_dem.update_particles(particles);
